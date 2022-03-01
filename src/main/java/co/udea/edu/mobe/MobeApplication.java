97a7496e8799911560e50df9c216142fc0955c54
CaroGomez <astridc.diaz@udea.edu.co>
2022-02-28 21:48:37 -0500

    Configuración inicial backend

---
+++ src/main/java/co/udea/edu/mobe/MobeApplication.java
@@ -0,0 +1,13 @@
+package co.udea.edu.mobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/udea/edu/mobe/MobeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.udea.edu.mobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
