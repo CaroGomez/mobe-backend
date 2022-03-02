3ecf3b519b4dd65ea561db0bfd4d6f18e7d51669
Alexis Herrera <jalexis.herrera@udea.edu.co>
2022-03-01 22:50:59 -0500

    Create Rol.java

---
+++ src/main/java/co/udea/edu/mobe/entity/Rol.java
@@ -0,0 +1,33 @@
+package co.udea.edu.mobe.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="roles")
+public class Rol implements Serializable {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 20)
+    private String nombre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
